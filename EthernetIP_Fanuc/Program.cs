06eee93547128a5d4a8bf0b9ab16c0c89a6260e7
Ton van Sluisveld <tonvanSluisveld@csiportal.com>
2019-04-10 13:59:26 +0200

    Add project files.

---
+++ EthernetIP_Fanuc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthernetIP_Fanuc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
